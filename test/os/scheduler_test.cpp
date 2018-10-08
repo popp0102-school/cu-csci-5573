afe39e2a5326bcd801e6027b635243a2ba2f829f
jpoppler <jpoppler@getnotion.com>
2018-10-08 00:19:18 -0600

    dont use cmake when building unit tests

--- test/os/scheduler_test.cpp
+++ test/os/scheduler_test.cpp
@@ -1,12 +1,6 @@
 #include <gtest/gtest.h>
 
 TEST(BasicTest, Equality) {
-  ASSERT_EQ(1, 1);
+  ASSERT_EQ(0, 0);
 };
-
-
-int main(int argc, char **argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
 
