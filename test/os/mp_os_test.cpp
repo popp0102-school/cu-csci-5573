2477b1e5d77bc662eb6e224998033c02ec7914d9
Jason <popp0102@gmail.com>
2018-10-13 22:14:59 -0600

    lots of cleanup

--- test/os/mp_os_test.cpp
+++ test/os/mp_os_test.cpp
@@ -1,9 +1,15 @@
 #include <gtest/gtest.h>
 #include "mp_os.h"
 
-TEST(MP_OS, Execute) {
+TEST(MP_OS, run) {
   MP_OS *modulus_prime = new MP_OS();
 
-  EXPECT_NO_THROW(modulus_prime->Execute());
+  EXPECT_NO_THROW(modulus_prime->run());
+};
+
+TEST(MP_OS, mpthread_create) {
+  MP_OS *modulus_prime = new MP_OS();
+
+  EXPECT_NO_THROW(modulus_prime->mpthread_create());
 };
 
