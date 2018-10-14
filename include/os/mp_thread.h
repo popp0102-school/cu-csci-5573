6494f089115b3e542267b9fbd6e6b0eed769bc08
Jason <popp0102@gmail.com>
2018-10-14 16:20:20 -0600

    add necessary context data structure to MP_Thread

--- include/os/mp_thread.h
+++ include/os/mp_thread.h
@@ -1,8 +1,14 @@
 #ifndef MP_THREAD_H
 #define MP_THREAD_H
 
+#include <malloc.h>
+#include <ucontext.h>
+
+#define STACK_SIZE 1024 * 64
+
 class MP_Thread {
   private:
+    ucontext_t context;
 
   public:
     MP_Thread(void (*start_routine)());
