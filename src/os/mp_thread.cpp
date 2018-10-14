f0b5d79cdb8de1bd2da0e8f251b592383fa3e027
jpoppler <jpoppler@getnotion.com>
2018-10-14 16:20:20 -0600

    add necessary context data structure to MP_Thread

--- src/os/mp_thread.cpp
+++ src/os/mp_thread.cpp
@@ -1,5 +1,13 @@
 #include "mp_thread.h"
 
 MP_Thread::MP_Thread(void (*start_routine)()) {
+  getcontext(&context);
+
+  context.uc_link           = NULL;
+  context.uc_stack.ss_sp    = malloc(STACK_SIZE);
+  context.uc_stack.ss_size  = STACK_SIZE;
+  context.uc_stack.ss_flags = 0;
+
+  makecontext(&context, start_routine, 0);
 }
 
