65f5c57e40f7bf1b7b591b7a10ac265876e8b961
jpoppler <jpoppler@getnotion.com>
2018-10-20 14:55:25 -0600

    pass the schedule choice into the constructor

--- include/os/mp_scheduler.h
+++ include/os/mp_scheduler.h
@@ -7,14 +7,16 @@
 class MP_Scheduler {
   public:
     enum schedule { FCFS, ROUND_ROBIN };
-    MP_Scheduler(MP_Thread *);
-    void run(schedule);
+    MP_Scheduler(MP_Thread *, schedule);
+    void run();
     void add_thread(MP_Thread*);
 
   private:
     std::queue<MP_Thread*> m_ready_queue;
-    void execute_thread(MP_Thread*);
     MP_Thread *m_main_thread;
+    schedule m_algo;
+
+    void execute_thread(MP_Thread*);
     void fcfs();
     void round_robin();
 };
