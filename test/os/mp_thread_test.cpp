4113357d0ff694b2ebc670057b1898f9e7f9dfb8
Jason <popp0102@gmail.com>
2018-10-20 12:40:55 -0600

    implement get_context with tests

--- test/os/mp_thread_test.cpp
+++ test/os/mp_thread_test.cpp
@@ -1,6 +1,15 @@
 #include <gtest/gtest.h>
 #include "mp_thread.h"
 
-TEST(MP_Thread, constructor) {
-  EXPECT_EQ(1, 1);
+void some_dummy_method() {}
+
+TEST(MP_Thread, default_constructor) {
+  MP_Thread *some_thread = new MP_Thread();
+  EXPECT_NO_THROW(some_thread->get_context());
 };
+
+TEST(MP_Thread, non_default_constructor) {
+  MP_Thread *some_thread = new MP_Thread(some_dummy_method);
+  EXPECT_NO_THROW(some_thread->get_context());
+};
+
