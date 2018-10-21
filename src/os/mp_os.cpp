3a54b110048a8c4e1b53dfeb09a9b8b00d811671
jpoppler <jpoppler@getnotion.com>
2018-10-20 20:36:25 -0600

    interrupter -> dispatcher. cleanup

--- src/os/mp_os.cpp
+++ src/os/mp_os.cpp
@@ -3,7 +3,7 @@
 MP_OS::MP_OS(MP_Scheduler::schedule algo) {
   m_main_thread = new MP_Thread();
   mp_scheduler = new MP_Scheduler(m_main_thread, algo);
-  init_interrupter(mp_scheduler);
+  init_dispatcher(mp_scheduler);
 }
 
 void MP_OS::thread_create(void (*start_routine)()) {
@@ -12,7 +12,7 @@ void MP_OS::thread_create(void (*start_routine)()) {
 }
 
 void MP_OS::run() {
-  run_interrupter();
+  run_dispatcher();
   mp_scheduler->run();
 }
 
