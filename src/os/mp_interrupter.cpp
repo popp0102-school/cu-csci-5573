cc9c00bb16aa9004d6494286c6c754531a80329f
Jason <popp0102@gmail.com>
2018-10-20 20:34:30 -0600

    seprate init and run

--- src/os/mp_interrupter.cpp
+++ src/os/mp_interrupter.cpp
@@ -19,10 +19,10 @@ void init_interrupter(MP_Scheduler *mp_sched) {
   it.it_interval.tv_usec = 50000;
   it.it_value.tv_sec = 1;
   it.it_value.tv_usec = 100000;
-  setitimer(ITIMER_REAL, &it, NULL);
 }
 
 void run_interrupter() {
+  setitimer(ITIMER_REAL, &it, NULL);
 }
 
 void context_switch(int i) {
