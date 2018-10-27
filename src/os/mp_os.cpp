fb9529c70e0fccaaf5ca668b4618eb5a00607659
Jason <popp0102@gmail.com>
2018-10-27 11:41:02 -0600

    add_thread -> add_ready

--- src/os/mp_os.cpp
+++ src/os/mp_os.cpp
@@ -3,12 +3,13 @@
 MP_OS::MP_OS(MP_Scheduler::schedule algo) {
   m_os_thread  = new MP_Thread();
   mp_scheduler = new MP_Scheduler(m_os_thread, algo);
+
   init_dispatcher(mp_scheduler);
 }
 
 void MP_OS::thread_create(void (*start_routine)()) {
   MP_Thread *thread = new MP_Thread(start_routine, m_os_thread);
-  mp_scheduler->add_thread(thread);
+  mp_scheduler->add_ready(thread);
 }
 
 void MP_OS::run() {
