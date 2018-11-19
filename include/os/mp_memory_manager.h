edbfc29318bd9b24a4dbd1f67a4c1e3aed0a635a
jpoppler <jpoppler@getnotion.com>
2018-11-18 19:51:31 -0700

    mp_memory_manager implemented

---
+++ include/os/mp_memory_manager.h
@@ -0,0 +1,20 @@
+#ifndef mp_MenMan_h
+#define mp_MenMan_h
+
+#include "mp_thread.h"
+#include <map>
+#include <string>
+#include <iostream>
+#include <iterator>
+
+class MP_MemoryManager{
+  public:
+    void* allocate(int numbytes, MP_Thread *mp_thread);
+    void deallocate(MP_Thread *mp_thread);
+
+  private:
+    std::map <std::string, void *> mp_memory_map;
+};
+
+#endif
+
