6a48f76076660db9864b210465bd870fa8f43cdb
jpoppler <jpoppler@getnotion.com>
2018-12-01 15:23:09 -0700

    cleanup and add m_quantum field to mp_thread

--- include/os/mp_thread.h
+++ include/os/mp_thread.h
@@ -7,23 +7,27 @@
 #include <iostream>
 #define STACK_SIZE 1024 * 64
 
+using namespace std;
+
 class MP_Thread {
   public:
     enum MP_Status { RUNNING, WAITING, FINISHED };
 
     MP_Thread();
-    MP_Thread(void (*start_routine)(), MP_Thread *main_thread, std::string label);
+    MP_Thread(void (*start_routine)(), MP_Thread *main_thread, string label);
+
     ucontext_t* get_context();
     MP_Status get_status();
     void set_status(MP_Status);
     void swap(MP_Thread*);
-    std::string getLabel();
-    friend std::ostream& operator<<(std::ostream& os, const MP_Thread& mp_thread);
+    string getLabel();
+    friend ostream& operator<<(ostream& os, const MP_Thread& mp_thread);
 
   private:
     ucontext_t m_context;
     MP_Status m_status;
-    std::string label;
+    string m_label;
+    int m_quantum;
 };
 
 #endif
