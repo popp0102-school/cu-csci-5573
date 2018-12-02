0ad8b9ccc64e8b9cd673810118ad7c9f298d9a7f
Jason <popp0102@gmail.com>
2018-12-01 17:56:42 -0700

    cleanup

--- include/lib/modulus-prime.h
+++ include/lib/modulus-prime.h
@@ -4,8 +4,10 @@
 #include "mp_os.h"
 #include <string>
 
-void mp_init(MP_Scheduler::schedule, int, std::string);
-void mp_add_thread(void (*thread_routine)(), std::string label);
+using namespace std;
+
+void mp_init(MP_Scheduler::schedule, int, string);
+void mp_add_thread(void (*thread_routine)(), string label);
 void mp_wait();
 void* mp_malloc(int);
 void mp_free(void *);
--- src/lib/modulus-prime.cpp
+++ src/lib/modulus-prime.cpp
@@ -2,11 +2,11 @@
 
 MP_OS *mp_os = NULL;
 
-void mp_init(MP_Scheduler::schedule algo, int usecs_quantum, std::string filename) {
+void mp_init(MP_Scheduler::schedule algo, int usecs_quantum, string filename) {
   mp_os = new MP_OS(algo, usecs_quantum, filename);
 }
 
-void mp_add_thread(void (*thread_routine)(), std::string label) {
+void mp_add_thread(void (*thread_routine)(), string label) {
   mp_os->thread_create(thread_routine, label);
 }
 
